7a0458cc840c9ba04a814482bac3cd230f9a6db5
ayloulheba <ayloulheba@gmail.com>
2024-03-19 20:23:19 +0300

    edit settings

--- taskapi/settings.py
+++ taskapi/settings.py
@@ -29,7 +29,7 @@ DEBUG = config("MY_DEBUG", "False")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
